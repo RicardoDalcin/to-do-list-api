2a859ca6fd23d314aa23a73fde7396a42543abdf
Ricardo <ricardo.dalcinh@gmail.com>
2019-12-14 19:16:21 -0300

    Automatic import of controllers

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-require('./app/controllers/authController')(app)
-require('./app/controllers/taskController')(app)
+require('./app/controllers')(app)
 
 app.listen(3001, () => {
   console.log('Listening on port 3001')
