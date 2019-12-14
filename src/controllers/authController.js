e1866b862a46a08298c94f97fd7faa10620868ac
Ricardo <ricardo.dalcinh@gmail.com>
2019-12-14 18:43:57 -0300

    User authentication and secret key

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth.json')
 
 const User = require('../models/User')
 
@@ -22,6 +25,27 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     return res.status(400).send({ error: 'Oops... there was an error with your registration!' })
   }
+})
+
+router.post('/authenticate', async (req, res) => {
+  const { login, password } = req.body
+
+  const user = await User.findOne({ $or: [{ 'email': login }, { 'username': login }] }).select('+password')
+
+  if (!user)
+    return res.status(400).send({ error: 'Oops... user was not found!' })
+
+  if (!await bcrypt.compare(password, user.password))
+    return res.status(401).send({ error: 'The username or pasword provided is invalid!' })
+
+  user.password = undefined
+
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 86400
+  })
+
+  res.send({ token })
+
 })
 
 module.exports = app => app.use('/auth', router)
