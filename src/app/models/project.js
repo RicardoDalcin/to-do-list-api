9829d7ff2fdad64cb5c8f08ed67d7df3f5a47b7e
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2019-12-20 15:41:15 -0300

    Add color to project

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -1,27 +1,33 @@
 const mongoose = require('../../database')
 
 const ProjectSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  tasks: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Task'
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	tasks: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Task'
+		}
+	],
+	color: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
 })
 
 const Project = mongoose.model('Project', ProjectSchema)
