a8989dbe1f9bf049f11ab86e6cf687d7b38f5a75
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2019-12-22 21:11:26 -0300

    Task description not required

--- src/app/models/task.js
+++ src/app/models/task.js
@@ -1,33 +1,33 @@
 const mongoose = require('../../database')
 
 const TaskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  project: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Project',
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+	title: {
+		type: String,
+		required: true
+	},
+	project: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Project',
+		required: true
+	},
+	description: {
+		type: String,
+		required: false
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	completed: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
 })
 
 const Task = mongoose.model('Task', TaskSchema)
